c8e409be96f551acc6ae5b2865f6ce86f55f1d09
Alucardlol <golodhom@gmail.com>
2017-03-17 01:27:19 +0300

    hw1

---
+++ app.js
@@ -0,0 +1,10 @@
+var mas1 = prompt('первая строка: ',0).toLowerCase().split('').sort().join();
+var mas2 = prompt('вторая строка: ',0).toLowerCase().split('').sort().join();
+
+if (mas1 === mas2)
+{
+	alert("строки являются анаграммами");
+} else
+{
+	alert("строки являются анаграммами");
+}
